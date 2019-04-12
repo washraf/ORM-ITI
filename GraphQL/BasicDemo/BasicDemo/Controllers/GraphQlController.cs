587974a3a32aaa680add0c52956989e3451b47e9
Walid Ashraf <eng_walid_ashraf@hotmail.com>
2019-04-12 19:57:01 +0200

    Basic GraphQL Demo

---
+++ GraphQL/BasicDemo/BasicDemo/Controllers/GraphQlController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using BasicDemo;
+using GraphQL;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicDemo.Controllers
+{
+    [Route(Startup.GraphQlPath)]
+    public class GraphQlController : Controller
+    {
+        public GraphQlController()
+        {
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] GraphQlQuery query)
+        {
+            var schema = new Schema { Query = new HelloWorldQuery() };
+            var result = await new DocumentExecuter().ExecuteAsync(x =>
+            {
+                x.Schema = schema;
+                x.Query = query.Query;
+                x.Inputs = query.Variables;
+            });
+            if (result.Errors?.Count > 0)
+            {
+                return BadRequest();
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ GraphQL/BasicDemo/BasicDemo/HelloWorldQuery.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Types;
+
+namespace BasicDemo
+{
+    public class HelloWorldQuery : ObjectGraphType
+    {
+        public HelloWorldQuery()
+        {
+            Field<StringGraphType>(
+                name: "hello",
+                resolve: context => "world"
+            );
+            Field<StringGraphType>(
+                name: "howdy",
+                resolve: context => "universe"
+            );
+        }
+
+    }
+}
